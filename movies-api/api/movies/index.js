7ebaeda7164b1a6ab33c42ba35a445b137ed5c0a
Allen Terescenco <allenterescencobq@gmail.com>
2021-12-04 15:58:11 +0000

    added routing for movies and movie reviews

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,3 +1,4 @@
+import uniqid from 'uniqid'
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 
@@ -19,5 +20,36 @@ router.get('/:id', (req, res) => {
     }
 });
 
+// Get movie reviews
+router.get('/:id/reviews', (req, res) => {
+    const id = parseInt(req.params.id);
+    // find reviews in list
+    if (movieReviews.id == id) {
+        res.status(200).json(movieReviews);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
+//Post a movie review
+router.post('/:id/reviews', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (movieReviews.id == id) {
+        req.body.created_at = new Date();
+        req.body.updated_at = new Date();
+        req.body.id = uniqid();
+        movieReviews.results.push(req.body); //push the new review onto the list
+        res.status(201).json(req.body);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
 
 export default router;
